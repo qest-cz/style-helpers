61e266d9b7967b79d216fdb2aa4f3819eced9194
Lukas Vaic <lukas.vaic@qest.cz>
2019-08-16 15:11:52 +0200

    🎉 initial commit

---
+++ README.md
@@ -0,0 +1,90 @@
+# @qest-cz/style-helpers
+
+Set of helper functions for easier work with styled components.
+
+## Preferred usage
+
+You should import `createStyleHelpers` and pass your `Theme` interface into it and then use created object.
+
+```tsx
+// style-helpers.ts
+import createStyleHelpers from '@qest/style-helpers';
+
+const initializedStyleHelpers = createStyleHelpers<Theme>();
+
+export const { color, resolution, util, baseUnit, transition, cssCondition, media } = initializedStyleHelpers; 
+```
+
+```tsx
+// styled.ts
+import styled from 'styled-components';
+import { color, media, baseUnit } from '../style-helpers';
+
+export const Heading = styled.h1`
+    color: ${color("blue")};
+    ${media.min(
+        'screenMdMin',
+        css`
+            margin-bottom: ${baseUnit(2)};
+        `,
+    )};
+`;
+```
+
+If you don't care about care about types, you can import functions directly from the package.
+
+```tsx
+import { color, resolution, util, baseUnit, transition, cssCondition, media } from '@qest/style-helpers';
+```
+
+
+## Available functions
+
+-   color
+-   resolution
+-   util
+-   baseUnit
+-   transition
+-   cssCondition
+-   media
+
+## Expected theme format
+
+Selectors expect this format of theme.
+
+```tsx
+const SCREEN_XLL = 1600;
+const SCREEN_XL = 1200;
+const SCREEN_LG = 992;
+const SCREEN_MD = 768;
+const SCREEN_SM = 576;
+const SCREEN_XS = 480;
+
+const theme = {
+    colors: {
+        white: '#fff',
+    },
+    resolution: {
+        screenLg: `${SCREEN_LG}px`,
+        screenLgMax: `${SCREEN_XL - 1}px`,
+        screenLgMin: `${SCREEN_LG}px`,
+        screenMd: `${SCREEN_MD}px`,
+        screenMdMax: `${SCREEN_LG - 1}px`,
+        screenMdMin: `${SCREEN_MD}px`,
+        screenSm: `${SCREEN_SM}px`,
+        screenSmMax: `${SCREEN_MD}px`,
+        screenSmMin: `${SCREEN_SM}px`,
+        screenXl: `${SCREEN_XL}px`,
+        screenXlMax: `${SCREEN_XLL - 1}px`,
+        screenXlMin: `${SCREEN_XL}px`,
+        screenXll: `${SCREEN_XLL}px`,
+        screenXllMin: `${SCREEN_XLL}px`,
+        screenXs: `${SCREEN_XS}px`,
+        screenXsMax: `${SCREEN_SM - 1}px`,
+        screenXsMin: `${SCREEN_XS}px`,
+    },
+    utils: {
+        baseUnit: 8,
+    },
+};
+```
---
+++ src/index.ts
@@ -0,0 +1,91 @@
+import get from 'lodash/get';
+import { css, FlattenInterpolation } from 'styled-components';
+
+interface GeneralTheme {
+    colors: any;
+    resolution: any;
+    utils: any;
+}
+
+const createStyledHelpers = <T extends GeneralTheme>() => {
+    type ResolutionKey = keyof T['resolution'];
+    type PropsWithTheme = { theme: T };
+    type Styles = FlattenInterpolation<PropsWithTheme>;
+
+    return {
+        color: (colorKey: keyof T['colors']) => ({ theme }: PropsWithTheme): number => {
+            return get(theme, ['colors', colorKey]);
+        },
+        resolution: (resolutionKey: keyof T['resolution']) => ({
+            theme,
+        }: PropsWithTheme): string => {
+            return get(theme, ['resolution', resolutionKey]);
+        },
+        util: (utilKey: keyof T['utils']) => ({
+            theme,
+        }: PropsWithTheme): string | number | ((...params: any) => string | number) => {
+            return get(theme, ['utils', utilKey]);
+        },
+        baseUnit: (multiplier = 1) => ({ theme }: PropsWithTheme): string => {
+            const selectedUnit = get(theme, ['utils', 'baseUnit']) as number;
+
+            return `${selectedUnit * multiplier}px`;
+        },
+        media: {
+            min: (breakpoint: ResolutionKey, styles: Styles) => ({ theme }: PropsWithTheme) => {
+                return css`
+                    @media (min-width: ${theme.resolution[breakpoint]}) {
+                        ${styles}
+                    }
+                `;
+            },
+            max: (breakpoint: ResolutionKey, styles: Styles) => ({ theme }: PropsWithTheme) => {
+                return css`
+                    @media (max-width: ${theme.resolution[breakpoint]}) {
+                        ${styles}
+                    }
+                `;
+            },
+            between: ([minBreakpoint, maxBreakpoint]: ResolutionKey[], styles: Styles) => ({
+                theme,
+            }: PropsWithTheme) => {
+                return css`
+                    @media (min-width: ${theme.resolution[minBreakpoint]}) and (max-width: ${theme
+                            .resolution[maxBreakpoint]}) {
+                        ${styles}
+                    }
+                `;
+            },
+        },
+        transition: (config?: { duration?: number; properties?: string; animation?: string }) => {
+            const {
+                duration = 0.3,
+                properties = 'all',
+                animation = 'cubic-bezier(0.215, 0.61, 0.355, 1)',
+            } = config || {};
+            return css`
+                transition: ${properties} ${duration}s ${animation};
+            `;
+        },
+        cssCondition: (condition: boolean, styles: Styles) => {
+            if (condition) {
+                return styles;
+            }
+            return null;
+        },
+    };
+};
+
+const initializedStyleHelpers = createStyledHelpers();
+
+export const {
+    color,
+    resolution,
+    util,
+    baseUnit,
+    transition,
+    cssCondition,
+    media,
+} = initializedStyleHelpers;
+
+export default createStyledHelpers;
